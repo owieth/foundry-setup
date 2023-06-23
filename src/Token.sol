bfc192d671aa8aae49c0ec35d2afbd005a48c458
Olivier <olivier.winkler@icloud.com>
2023-06-23 11:38:12 +0200

    feat(contract): add some more documentation to contract

--- src/Token.sol
+++ src/Token.sol
@@ -8,15 +8,115 @@ import {Ownable} from "@oz/access/Ownable.sol";
 /// @author Olivier Winkler (https://github.com/owieth)
 /// @custom:security-contact xxx@gmail.com
 contract Token is ERC20, Ownable {
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Explain to a developer any extra details
+    error Token__Error();
+
+    /// @dev Explain to a developer any extra details
+    /// @param id a parameter just like in doxygen (must be followed by parameter name)
+    error Token__ErrorWithParams(uint256 id);
+
+    /*//////////////////////////////////////////////////////////////
+                                 STRUCTS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Explain to a developer any extra details
+    /// @param id a parameter just like in doxygen (must be followed by parameter name)
+    /// @param name a parameter just like in doxygen (must be followed by parameter name)
+    struct TokenStruct {
+        uint256 id;
+        string name;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                CONSTANTS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Explain to a developer any extra details
+    uint256 private constant CONSTANT_NUMBER = 100;
+
+    /*//////////////////////////////////////////////////////////////
+                                IMMUTABLES
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Explain to a developer any extra details
+    uint256 private immutable i_immutableNumber = 100;
+
+    /*//////////////////////////////////////////////////////////////
+                                 STORAGE
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Explain to a developer any extra details
+    uint256 public s_number;
+
+    /*//////////////////////////////////////////////////////////////
+                                EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Explain to a developer any extra details
+    /// @param from a parameter just like in doxygen (must be followed by parameter name)
+    /// @param to a parameter just like in doxygen (must be followed by parameter name)
+    /// @param id a parameter just like in doxygen (must be followed by parameter name)
+    event TokenEvent(address indexed from, address indexed to, address indexed id);
+
+    /*//////////////////////////////////////////////////////////////
+                                MODIFIERS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Explain to a developer any extra details
+    modifier onlyMinter() {
+        _;
+    }
+
+    /// @dev Explain to a developer any extra details
+    /// @param minter a parameter just like in doxygen (must be followed by parameter name)
+    modifier onlyMinterByAddress(address minter) {
+        _;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                               CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
     constructor() ERC20("Token", "TKN") {}
 
-    uint256 public number;
+    /*//////////////////////////////////////////////////////////////
+                               EXTERNAL
+    //////////////////////////////////////////////////////////////*/
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @param newNumber a parameter just like in doxygen (must be followed by parameter name)
+    function setNumber(uint256 newNumber) external {
+        s_number = newNumber;
     }
 
-    function increment() public {
-        number++;
+    /*//////////////////////////////////////////////////////////////
+                               PUBLIC
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    function incrementNumber() public {
+        s_number++;
     }
+
+    /*//////////////////////////////////////////////////////////////
+                               INTERNAL
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    function _calculateNumber() internal {}
+
+    /*//////////////////////////////////////////////////////////////
+                               PRIVATE
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    function _calculateAmount() private {}
 }
